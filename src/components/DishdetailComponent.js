c877e61979d57709dd52e15cd7bf836afb1b0314
siyuan <pengsiyuan666@gmail.com>
2020-06-03 10:34:14 +0800

    Presentational and Container Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -3,9 +3,33 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class Dishdetail extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {}
+    renderDish(dish) {
+        if (dish != null) {
+            return (
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1">
+                        <Card>
+                            <CardImg width="100%" src={dish.image} alt={dish.name}></CardImg>
+                            <CardBody>
+                                <CardTitle>{dish.name}</CardTitle>
+                                <CardText>{dish.description}</CardText>
+                            </CardBody>
+                        </Card>
+                    </div>
+                    <div className="col-12 col-md-5 m-1">
+                        <h4>Comments</h4>
+                        <ul>
+                            {this.renderComments(dish.comments)}
+                        </ul>
+                    </div>
+                </div>
+            );
+        }
+        else {
+            return (
+                <div></div>
+            );
+        }
     }
 
     renderComments(comments) {
@@ -14,7 +38,7 @@ class Dishdetail extends Component {
                 return (
                     <li key={comment.id}>
                         <p>{comment.comment}</p>
-                        <p>-- {comment.author}, {comment.date}</p>
+                        <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</p>
                     </li>
                 );
             });
@@ -27,25 +51,9 @@ class Dishdetail extends Component {
     }
 
     render() {
-        const dish = this.props.dish;
-
         return (
-            <div className="row">
-                <div className="col-12 col-md-5 m-1">
-                    <Card>
-                        <CardImg width="100%" src={dish.image} alt={dish.name}></CardImg>
-                        <CardBody>
-                            <CardTitle>{dish.name}</CardTitle>
-                            <CardText>{dish.description}</CardText>
-                        </CardBody>
-                    </Card>
-                </div>
-                <div className="col-12 col-md-5 m-1">
-                    <h4>Comments</h4>
-                    <ul>
-                        {this.renderComments(dish.comments)}
-                    </ul>
-                </div>
+            <div className="container">
+                {this.renderDish(this.props.dish)}
             </div>
         );
     }
