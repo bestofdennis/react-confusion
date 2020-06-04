1bd6ed892a332cd1ba4145d5cb36a235fc4ea695
siyuan <pengsiyuan666@gmail.com>
2020-06-04 09:52:31 +0800

    Introduce About Us page

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,6 +3,7 @@ import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Contact from './ContactComponent';
 import DishDetail from './DishdetailComponent';
+import About from './AboutComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
@@ -34,12 +35,28 @@ class Main extends Component {
       );
     }
 
+    const DishWithId = ({match}) => {
+      return (
+        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
+          comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+          />
+      );
+    }
+
+    const AboutUs = () => {
+      return (
+        <About leaders={this.state.leaders} />
+      );
+    }
+
     return (
       <div>
         <Header />
         <Switch>
           <Route path="/home" component={HomePage} />
           <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+          <Route path="/menu/:dishId" component={DishWithId} />
+          <Route path="/aboutus" component={AboutUs} />
           <Route exact path="/contactus" component={Contact} />
           <Redirect to="/home" />
         </Switch>
